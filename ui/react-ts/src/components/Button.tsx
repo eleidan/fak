0635fc654d915cfa0eb77525e39b500587386cd6
Oleg Kulyk <helh.saintman@gmail.com>
2021-04-09 23:44:12 +0300

    Add storybook

---
+++ ui/react-ts/src/components/Button.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './button.css';
+
+export interface ButtonProps {
+  primary?: boolean;
+  backgroundColor?: string;
+  size?: 'small' | 'medium' | 'large';
+  label: string;
+  onClick?: () => void;
+}
+
+//export const Button: React.FC<ButtonProps> = ({
+export const Button = ({
+  primary = false,
+  size = 'medium',
+  backgroundColor,
+  label,
+  ...props
+}) => {
+  const mode = primary ? 'button--primary' : 'button--secondary';
+  return (
+    <button
+      type="button"
+      className={['button', `button--${size}`, mode].join(' ')}
+      style={{backgroundColor}}
+      {...props}
+    >
+      {label}
+    </button>
+  );
+};
